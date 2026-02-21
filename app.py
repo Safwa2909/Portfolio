7ec0c827d6f6eb40952198a1a4887cd0b15d5c84
Safwa M <safss2909@gmail.com>
2026-02-21 06:01:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import sqlite3
+import os
 
 app = Flask(__name__)
 
@@ -25,5 +26,7 @@ def home():
         projects=projects
     )
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
